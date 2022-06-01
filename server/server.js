2edd44f8a3edc4c59835c06052fb5368bfd47cb3
Mathew Street <mathew.k.street@gmail.com>
2022-06-01 16:25:20 +0100

    routes updated

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const express = require('express');
+
+const path = require('path');
 const logger = require('morgan');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/config.json')[env];
+
 
 const indexRouter = require('./routes/index-routes.js');
 
 
 const app = express();
 
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(helmet());
+app.use(cookieParser());
+
+app.use('/', indexRouter);
 
 const PORT = (process.env.PORT || '3000');
 
