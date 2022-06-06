1ac90b0d5d5f81d2211cb44b364ffab39b77ab9c
Mathew Street <mathew.k.street@gmail.com>
2022-06-06 15:55:57 +0100

    Photo services Updated

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ const logger = require('morgan');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config.json')[env];
 
 
 const indexRouter = require('./routes/index-routes.js');
+const photoRouter = require('./routes/photos.js');
 
 
 const app = express();
@@ -22,6 +24,7 @@ app.use(helmet());
 app.use(cookieParser());
 
 app.use('/', indexRouter);
+app.use('/photos', photoRouter);
 
 const PORT = (process.env.PORT || '3000');
 
